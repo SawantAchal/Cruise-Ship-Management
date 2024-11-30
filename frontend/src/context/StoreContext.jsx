77e20d7b77fda59a5b3b5861b5d7c422c63e80fa
SawantAchal <achals1718@gmail.com>
2024-11-30 22:59:11 +0530

    add backedn url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider  = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = 'http://localhost:4000'
+  const url = 'https://cruise-ship-management.onrender.com'
   const [token ,setToken] = useState('')
   const [menuData , setMenuData]= useState([])
   const [stationeryData , setstationeryData]= useState([])
